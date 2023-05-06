62c19d775bdfe09717d938579a0c384503406ca8
DanilaKuznetcov <iam.adamant@mail.ru>
2023-05-06 18:18:11 +0300

    Test structure

--- server/main.py
+++ server/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
@@ -6,10 +8,22 @@ app = FastAPI()
 def home():
     return {'message': "Антон - гей"}
 
-@app.get('/category/')
-def get_iems_by_category():
-    return {'message': "Антон - гей"}    
+@app.get('/category_{category_id}/product_{prod_id}/hero_{hero_id}')
+def get_item(category_id:int, prod_id:int, hero_id:int):
+    res = jsonable_encoder({'category': category_id, 'product': prod_id, 'hero': hero_id})
+    return JSONResponse(content=res)
 
-@app.get('/items/')
-def get_iems_by_type():
+
+#filters
+@app.get('/category_{category_id}/')
+def get_iems_by_category(category_id: int):
+    return {'message': "Антон - гей"}
+
+@app.get('/product_{product_id}/')
+def get_iems_by_product(product_id):
+    return {'message': "Антон - гей"}
+
+
+@app.get('/hero_{hero_id}/')
+def get_iems_by_product(hero_id):
     return {'message': "Антон - гей"}
