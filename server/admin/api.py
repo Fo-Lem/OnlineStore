d45b32a074142ec5110e52cf4a1e9de60599f1ff
DanilaKuznetcov <iam.adamant@mail.ru>
2023-05-11 21:20:01 +0300

    Create module for admin panel

---
+++ server/admin/api.py
@@ -0,0 +1,15 @@
+def get_categories():
+    pass
+
+def get_product_types():
+    pass
+
+def get_heroes():
+    pass
+
+def get_clients():
+    pass
+
+def get_products():
+    '''Returns union result of slq JOIN "identity" and "item"'''
+    pass
