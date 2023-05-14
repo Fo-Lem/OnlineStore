e0af8bc24a1d9c55542fcd1ca4aed318fb93ad4b
DanilaKuznetcov <iam.adamant@mail.ru>
2023-05-14 19:29:26 +0300

    Add starting script

--- server/scripts/start.sh
+++ server/scripts/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-source /home/adamant/Desktop/project/OnlineStore/server/onlst/bin/activate
-/home/adamant/Desktop/project/OnlineStore/server/onlst/bin/gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind localhost:8080
+source //home/nginxadmin/OnlineStore/server/onlst/bin/activate
+/home/nginxadmin/OnlineStore/server/onlst/bin/gunicorn main:app --workers 4 --worker-class uvicorn.workers.UvicornWorker --bind localhost:8080
